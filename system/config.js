92d80afbc2422dbcdffa1558c9ff37f71bec4819
James Official🇰🇪 <jamesoffkev20@gmail.com>
2026-06-08 20:46:35 +0300

    Add files via upload

---
+++ system/config.js
@@ -0,0 +1,17 @@
+//~~~~~~~~~ Setting Owner~~~~~~~~~~//
+global.prefix = [".", "!", ".", ",", "🐤", "🗿",""]; 
+global.publik = true
+global.owner = ["6285891327504"] 
+global.namabot = 'InfinityX'
+//~~~~~~~~~ Setting ~~~~~~~~~~//
+global.namach = "SanXoffc|| Information"
+global.link = "https://whatsapp.com/channel/0029VbCgLoH1yT2ATiT5zC2a"
+global.idch = "120363419780317300@newsletter"
+global.imgthumb = "https://img2.pixhost.to/images/8211/731563769_ochobot.jpg"
+//~~~~~~~~~ Setting Mess ~~~~~~~~~~//
+global.mess = { 
+owner: '𝖪𝗁𝗎𝗌𝗎𝗌 𝖮𝗐𝗇𝖾e𝗋',
+premium: '𝖪𝗁𝗎𝗌𝗎𝗌 𝖯𝗋𝖾𝗆𝗂𝗎𝗆',
+succes: '𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅'
+//~~~~~~~~~ End ~~~~~~~~~~//
+}
